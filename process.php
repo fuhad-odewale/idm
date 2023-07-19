4d173b1340b746bff761090b06343d9850146cf5
fuhad odewale <fuhaodewale@gmail.com>
2023-07-19 13:15:56 +0100

    change name

--- process.php
+++ process.php
@@ -1,31 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>idm</title>
-</head>
-<body>
-<h1>Contact Form</h1>
-  <form id="contactForm">
-    <label for="name">Name:</label>
-    <input type="text" id="name" name="name" required><br>
-
-    <label for="dob">Date of Birth:</label>
-    <input type="date" id="dob" name="dob" required><br>
-
-    <label for="email">Email Address:</label>
-    <input type="email" id="email" name="email" required><br>
-
-    <label for="phone">Phone Number:</label>
-    <input type="tel" id="phone" name="phone" required><br>
-
-    <input type="submit" value="Submit">
-  </form>
-
-
-  <?php
+<?php
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
   // Get the form data from the request body
   $formData = json_decode(file_get_contents("php://input"), true);
@@ -56,8 +29,3 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
   }
 }
 ?>
-
-
-  <script src="submit.js"></script>
-</body>
-</html>
