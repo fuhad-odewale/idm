6745be13b31a135670a08af40c6749a8273b53c6
fuhad odewale <fuhaodewale@gmail.com>
2023-07-19 13:33:17 +0100

    update

--- process.php
+++ process.php
@@ -1,31 +1,26 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-  // Get the form data from the request body
-  $formData = json_decode(file_get_contents("php://input"), true);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $dob = $_POST["dob"];
+    $email = $_POST["email"];
+    $phone = $_POST["phone"];
 
-  // Set the recipient email address
-  $to = "fuhaodewale@gmail.com";
+    // Modify the following email address to the one you want to receive the form data
+    $to_email = "your_email@example.com";
+    $subject = "Form Submission - Contact Details";
+    $message = "Name: $name\n";
+    $message .= "Date of Birth: $dob\n";
+    $message .= "Email Address: $email\n";
+    $message .= "Phone Number: $phone\n";
 
-  // Set the email subject
-  $subject = "New Contact Form Submission";
+    // You can add additional headers or settings as required
+    $headers = "From: $email" . "\r\n";
 
-  // Build the email content
-  $message = "Name: " . $formData["name"] . "\n";
-  $message .= "Date of Birth: " . $formData["dob"] . "\n";
-  $message .= "Email Address: " . $formData["email"] . "\n";
-  $message .= "Phone Number: " . $formData["phone"] . "\n";
-
-  // Additional headers
-  $headers = "From: " . $formData["email"] . "\r\n";
-  $headers .= "Reply-To: " . $formData["email"] . "\r\n";
-
-  // Send the email
-  if (mail($to, $subject, $message, $headers)) {
-    // Return a success message if the email was sent successfully
-    echo json_encode(array("message" => "Form submission successful!"));
-  } else {
-    // Return an error message if there was a problem sending the email
-    echo json_encode(array("message" => "Error: Unable to send the email."));
-  }
+    // Send email
+    if (mail($to_email, $subject, $message, $headers)) {
+        echo "Thank you for your submission!";
+    } else {
+        echo "Error sending email. Please try again later.";
+    }
 }
 ?>
